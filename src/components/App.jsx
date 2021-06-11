a08557ec67cb048b5916b527977e0aee066b1c5d
AndyLinxies <linxies@gmail.com>
2021-06-11 21:02:04 +0200

    Udemy Practice props

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./car_component"
+import contacts from "../contacts"
+console.log(contacts[0].name);
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card name={contacts[0].name} src={contacts[0].imgURL} phone= {contacts[0].phone} email={contacts[0].email} />
+      
+      <Card name={contacts[1].name} src={contacts[1].imgURL} phone= {contacts[1].phone} email={contacts[1].email} />
+      
+      <Card name={contacts[2].name} src={contacts[2].imgURL} phone= {contacts[2].phone} email={contacts[2].email} />
+
+    </div>
+  );
+}
+
+export default App;
